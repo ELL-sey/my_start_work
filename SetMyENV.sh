ac81b60f1f7551ca0206609f5d92b6076401fe7f
ELL-sey <158863168+ELL-sey@users.noreply.github.com>
2024-11-12 08:28:34 +0700

    Update SetMyENV.sh
    new answer
    + y

--- SetMyENV.sh
+++ SetMyENV.sh
@@ -10,26 +10,26 @@ then
   exit 1
 fi
 
-
+yes_answ=['y Y ye YE Ye Yes YES YEs д Д да Да ДА нуы lf']
 
 echo "Поменять локаль на Русский?"
 read user_answ
 
-if [[ $user_answ  == y* ]];
+if [[ " ${yes_answ[*]} " =~ ${user_answ} ]];
 then
   sudo localectl set-locale LANG=ru_RU.UTF-8
 fi
 
 sudo timedatectl set-timezone Asia/Novosibirsk
-sudo apt install vim neovim curl git net-tools htop
+sudo apt install vim neovim curl git net-tools htop -y
 
 
 echo "Lazy Vim нужен?"
 read user_answ
 
-if [[ $user_answ  == y* ]];
+if [[ " ${yes_answ[*]} " =~ ${user_answ} ]];
 then
-sudo apt-get update && sudo apt-get install build-essential 
+sudo apt-get update && sudo apt-get install build-essential -y 
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 fi
@@ -47,7 +47,7 @@ rm aliases.txt
 echo "Нужо установить че нить?"
 read user_answ
 
-if [[ ! $user_answ  == y* ]];
+if [[ " ${yes_answ[*]} " =~ ${user_answ} ]];
 then
   exit 0
 fi
@@ -57,7 +57,7 @@ fi
 echo "Нужен Fail2ban?"
 read user_answ
 
-if [[ $user_answ  == y* ]];
+if [[ " ${yes_answ[*]} " =~ ${user_answ} ]];
 then
   sudo apt install fail2ban -y
 #vim /etc/fail2ban/jail.conf
@@ -67,7 +67,7 @@ fi
 echo "Нужен docker?"
 read user_answ
 
-if [[ $user_answ  == y* ]];
+if [[ " ${yes_answ[*]} " =~ ${user_answ} ]];
 then
   sudo apt update > /dev/null
   sudo apt install curl software-properties-common ca-certificates apt-transport-https -y
